fecdc695a0c13ea2953d340a3a02b6f6fb4fefed
nevein <neveinabdelfattah@gmail.com>
2022-08-14 21:45:05 +0200

    Added the functionality to distinguish the section in view

--- js/app.js
+++ js/app.js
@@ -28,25 +28,13 @@ const fragment = document.createDocumentFragment();
 const sections = document.querySelectorAll('section');
 const h2InSections = document.querySelectorAll('section h2');
 const theUl = document.getElementById('navbar__list');
+const theNav = document.querySelector('.page__header');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-// Populating li in the navbar
-for (let i = 0; i < sections.length; i++){
-    const newLi = document.createElement('li');
-    newLi.innerText = h2InSections[i].innerText;
-    newLi.classList.add('menu__link');
-    fragment.appendChild(newLi);
-
-}
-theUl.appendChild(fragment);
-
-
-
 
 
 /**
@@ -58,8 +46,46 @@ theUl.appendChild(fragment);
 // build the nav
 
 
+for (let i = 0; i < sections.length; i++){
+    const newLi = document.createElement('li');
+    const newA = document.createElement('a');
+    newA.innerText = h2InSections[i].innerText;
+    newLi.appendChild(newA);
+    newA.classList.add('menu__link');
+    fragment.appendChild(newLi);
+
+}
+theUl.appendChild(fragment);
+
 // Add class 'active' to section when near top of viewport
 
+window.addEventListener('scroll',function(){
+    // Number of pixels that the document is currently scrolled verticall
+    const windowTop = window.scrollY;
+    const navHeight = theNav.clientHeight;
+    let sectionTop;
+    sections.forEach((section)=>{
+        // Top position (in pixels) relative to the parent.
+        sectionTop = section.offsetTop;
+        sectionHeight = section.clientHeight;
+
+        if((Math.round(windowTop) + navHeight >= sectionTop) && (Math.round(windowTop) <= sectionTop + sectionHeight)){
+            section.classList.add('your-active-class');
+
+            // Remove the active class from every other section that is not on the viewport.
+            for (let j = 0; j < sections.length ; j++){
+                if (sections[j] === section){
+                    continue;
+                }
+                sections[j].classList.remove('your-active-class');
+            }
+        }
+
+    });
+
+        
+   
+});
 
 // Scroll to anchor ID using scrollTO event
 
